c7225a3ceabd5ac5e16c366fd1625546340b680c
Muhammad Afzal <ch.afzal646@gmail.com>
2022-09-15 15:26:42 +0500

    first commit

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+const options = {
+  params: {
+    maxResults: "50",
+  },
+  headers: {
+    "X-RapidAPI-Key": "b77631c066msh56121a0eaf268e0p1f9baajsn3392f8baa43f",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+};
